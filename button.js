6972454bff054a873fb36803fad1afce3e8d2f01
prashast99 <84674619+prashast99@users.noreply.github.com>
2021-05-23 21:06:14 +0530

    Initial Commit

---
+++ button.js
@@ -0,0 +1,64 @@
+function myFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "View more";
+    moreText.style.display = "none";
+  }
+  else {
+    dots.style.display = "none";
+    btnText.innerHTML = "View less";
+    moreText.style.display = "inline";
+  }
+}
+function myFunction2() {
+  var dots2 = document.getElementById("dots2");
+  var moreText2 = document.getElementById("more2");
+  var btnText = document.getElementById("myBtn2");
+
+  if (dots2.style.display === "none") {
+    dots2.style.display = "inline";
+    btnText.innerHTML = "View more";
+    moreText2.style.display = "none";
+  }
+  else {
+    dots2.style.display = "none";
+    btnText.innerHTML = "View less";
+    moreText2.style.display = "inline";
+  }
+}
+function myFunction3() {
+  var dot3 = document.getElementById("dots3");
+  var moreText3 = document.getElementById("more3");
+  var btnText = document.getElementById("myBtn3");
+
+  if (dots3.style.display === "none") {
+    dots3.style.display = "inline";
+    btnText.innerHTML = "View more";
+    moreText3.style.display = "none";
+  }
+  else {
+    dots3.style.display = "none";
+    btnText.innerHTML = "View less";
+    moreText3.style.display = "inline";
+  }
+}
+function myFunction4() {
+  var dot4 = document.getElementById("dots4");
+  var moreText4 = document.getElementById("more4");
+  var btnText = document.getElementById("myBtn4");
+
+  if (dots4.style.display === "none") {
+    dots4.style.display = "inline";
+    btnText.innerHTML = "View more";
+    moreText4.style.display = "none";
+  }
+  else {
+    dots4.style.display = "none";
+    btnText.innerHTML = "View less";
+    moreText4.style.display = "inline";
+  }
+}
